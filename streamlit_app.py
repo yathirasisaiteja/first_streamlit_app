743f150fe7d661db6ea06cddcd41a5197e7488e5
yathirasisaiteja <137860039+yathirasisaiteja@users.noreply.github.com>
2023-06-29 12:07:36 +0530

    Update streamlit_app.py
    added link to get fruits list

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 import pandas
-
+import requests
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -30,5 +30,7 @@ streamlit.header('🍌🥭 list of fruites selected 🥝🍇')
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 
 
