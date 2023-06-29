68a4bae4c2beb2c02ab3d3dda602080d246ac2e4
yathirasisaiteja <137860039+yathirasisaiteja@users.noreply.github.com>
2023-06-29 12:11:08 +0530

    Update streamlit_app.py
    remove 200 response and showed out put in json

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,9 @@ streamlit.header('🍌🥭 list of fruites selected 🥝🍇')
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
+streamlit.header("Fruityvice Fruit Advice!")
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
 
 
