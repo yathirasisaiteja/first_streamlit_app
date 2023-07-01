094850ce5be8d11a29a4ef3b91c513b2fc9806dc
yathirasisaiteja <137860039+yathirasisaiteja@users.noreply.github.com>
2023-07-01 14:45:18 +0530

    Update streamlit_app.py
    snow changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Apple'])
 #streamlit.text(fruits_selected)
 
 streamlit.dataframe(my_fruit_list)
