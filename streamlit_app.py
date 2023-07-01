26f2a832057ca5de397fd9926efdcdbb01948dab
yathirasisaiteja <137860039+yathirasisaiteja@users.noreply.github.com>
2023-07-01 15:57:02 +0530

    Update streamlit_app.py
    ddddd

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,15 +55,18 @@ for x in fruits_selected:
 streamlit.header("🍇 Fruityvice Fruit Advice!  🍇")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-streamlit.text(fruityvice_response.json())
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-try:
- fruityvice_normalized = fruityvice_normalized.set_index('name')
- streamlit.dataframe(fruityvice_normalized)
-except:
- streamlit.text('fruit '+x+' not present at the server')
+#streamlit.write('The user entered ', fruit_choice)
+if not fruit_choice:
+ streamlit.error("Please select a fruit to get information")
+else:
+ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+ streamlit.text(fruityvice_response.json())
+ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+ try:
+  fruityvice_normalized = fruityvice_normalized.set_index('name')
+  streamlit.dataframe(fruityvice_normalized)
+ except:
+  streamlit.text('fruit '+x+' not present at the server')
 
 
 #snow connector usage
